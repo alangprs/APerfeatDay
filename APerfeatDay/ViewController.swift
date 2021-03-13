f8ee363e08ae1e36d591693eabe17971f2dff3bc
will <alangprs1@gmail.com>
2021-03-13 19:52:53 +0800

    基本畫面完畢

--- APerfeatDay/ViewController.swift
+++ APerfeatDay/ViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  APerfeatDay
-//
-//  Created by 翁燮羽 on 2021/3/12.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    //連結名稱
+    
+    @IBOutlet var titleLabel: [UILabel]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        titleLabel[0].text = "靚旅國際旅行社"
+        titleLabel[1].text = "fb.com/APerfectDayTW"
     }
-
-
+//連結FB按鈕
+    @IBAction func fbButton(_ sender: UIButton) {
+        if let url = URL(string: "https://www.facebook.com/APerfectDayTW"){
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
 }
 
